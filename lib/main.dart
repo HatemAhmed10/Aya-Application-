cdfd2041e02ff99dd64897400d223850921f5698
Hatem <hatemahmedmohamed27@gmail.com>
2022-12-31 23:17:46 +0200

    3_Connect_toFirebase

--- lib/main.dart
+++ lib/main.dart
@@ -4,9 +4,14 @@ import 'package:aya/presentation/screens/1New_Design/Page1_Screen.dart';
 import 'package:bloc/bloc.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(MyApp());
 }
 
